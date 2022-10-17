fb562c86f9fa6836c5d99c16997d5d173a33ba9f
MinQan147 <TBNRQuanNguyen147@gmail.com>
2022-10-17 19:49:23 +0700

    Demo JS

---
+++ Buoi7_BKDL_14102022/hinhtron.js
@@ -0,0 +1,7 @@
+const PI = 3.14;
+let banKinh = prompt('Nhập bán kính');
+let chuVi = 2 * banKinh * PI;
+let dienTich = banKinh * banKinh * PI;
+document.write('Bán kính: ', banKinh);
+document.write(', Chu vi:', chuVi);
+document.write(', Diện tích:', dienTich);
